bc7a7da229e1be36cd9fc9a5ab819f30feda6721
jiazhen <fdf515@gmail.com>
2014-04-19 11:10:02 +0100

    destroy modal user and controller users, rebuild the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   resources :posts
   resources :sessions, only: [:new, :create, :destroy]
   resources :charges
@@ -9,7 +8,6 @@ Rails.application.routes.draw do
   match '/about'    => 'static_pages#about',    via: 'get'
   match '/contact'  => 'static_pages#contact',  via: 'get'
   match '/sitemap'  => 'static_pages#sitemap',  via: 'get'
-  match '/signup'   => 'users#new',             via: 'get'
   match '/signin'   => 'sessions#new',          via: 'get'
   match '/signout'  => 'sessions#destroy',      via: 'delete'
   
