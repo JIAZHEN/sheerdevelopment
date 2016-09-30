036b0cffd7004b7037a151845cd2f09e0298f29e
Joe Xie <fdf515@gmail.com>
2016-09-30 17:27:45 +0100

    Use gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
-//= require bootstrap.min
 //= require jquery.infinitescroll.min
 //= require imagesloaded.pkgd.min
 //= require home
