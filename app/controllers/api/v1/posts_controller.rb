d8d845a4408131021f47c7d0a496c4e5b6927509
Jiazhen Xie <fdf515@gmail.com>
2019-06-30 10:47:02 +0100

    Build the api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,12 @@
 module Api::V1
   class PostsController < ApiController
     def index
-      render json: Post.all
+      render json: Post.all.to_json
+    end
+
+    def show
+      @post = Post.find(params[:id])
+      render json: @post.to_json
     end
   end
 end
