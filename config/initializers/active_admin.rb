b9d1fd081ab905dc6a3b174b1b8ff1ddbb69884f
Jiazhen Xie <fdf515@gmail.com>
2019-06-30 10:06:21 +0100

    Completed the active admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -124,7 +124,7 @@ ActiveAdmin.setup do |config|
   # This allows your users to comment on any resource registered with Active Admin.
   #
   # You can completely disable comments:
-  # config.comments = false
+  config.comments = false
   #
   # You can change the name under which comments are registered:
   # config.comments_registration_name = 'AdminComment'
@@ -305,6 +305,7 @@ ActiveAdmin.setup do |config|
   # override the content of the footer here.
   #
   # config.footer = 'my custom footer text'
+  config.footer = "Sheerdevelopment"
 
   # == Sorting
   #
