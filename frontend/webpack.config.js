385c550294aca2ab8300eb6b90b675413e2c82ca
Jiazhen Xie <fdf515@gmail.com>
2019-06-30 15:54:15 +0100

    Integrate react, webpack, babel

---
+++ frontend/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HWP = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, '/src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: './dist',
+    proxy: [{
+      context: ['/v1', '/api'],
+      target: 'http://localhost:3001',
+    }]
+  },
+  plugins:[
+    new HWP({
+      template: path.join(__dirname, "./src/index.html"),
+      filename: "./index.html"
+    })
+  ],
+  watch: true
+};
