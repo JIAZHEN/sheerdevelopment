dabbc6b3b8c1d91961354d670e96a1eb3326e6ff
Jiazhen Xie <fdf515@gmail.com>
2019-07-06 14:31:59 +0100

    fallback requests to nodejs

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   ActiveAdmin.routes(self)
+
   scope module: 'api' do
     namespace :v1 do
       resources :posts
     end
+  end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
   end
 end
