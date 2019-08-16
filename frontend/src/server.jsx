612bfa2a0db3325b312f1bde7b63a9400eb3ed91
Jiazhen Xie <fdf515@gmail.com>
2019-08-16 20:49:06 +0100

    Update stylesheet and lang

--- frontend/src/server.jsx
+++ frontend/src/server.jsx
@@ -23,17 +23,23 @@ server
     } else {
       res.status(200).send(
         `<!doctype html>
-    <html lang="">
+    <html lang="en">
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
+        <title>Senior Software Engineer | Jiazhen Xie</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${
           assets.client.css
             ? `<link rel="stylesheet" href="${assets.client.css}">`
             : ''
         }
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
         ${
           process.env.NODE_ENV === 'production'
             ? `<script src="${assets.client.js}" defer></script>`
