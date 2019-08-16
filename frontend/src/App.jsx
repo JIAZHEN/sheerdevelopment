bb8b5b0b3e954a36df5770bb02adb9fdc528d85b
Jiazhen Xie <fdf515@gmail.com>
2019-08-16 20:34:16 +0100

    Use frontend SSR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history'
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 import Nav from './components/nav/nav';
 import Footer from './components/footer/footer';
 import Posts from './components/posts/posts';
@@ -14,20 +14,21 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 library.add(fab, faCheckSquare, faCoffee)
 
-ReactGA.initialize('UA-50154241-2');
+// ReactGA.initialize('UA-50154241-2');
 
-const history = createBrowserHistory();
-history.listen((location, action) => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
-});
+// const history = createBrowserHistory();
+// history.listen((location, action) => {
+//   ReactGA.set({ page: location.pathname });
+//   ReactGA.pageview(location.pathname);
+// });
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Nav />
-        <Switch history={history}>
+        {/*<Switch history={history}>*/}
+        <Switch>
           <Route exact path="/" component={Posts} />
           <Route exact path="/posts" component={Posts} />
           <Route exact path="/about" component={About} />
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import http from 'http';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+let app = require('./server').default;
+
+const server = http.createServer(app);
+
+let currentApp = app;
+
+server.listen(process.env.PORT || 3000, error => {
+  if (error) {
+    console.log(error);
+  }
+
+  console.log('🚀 started');
+});
+
+if (module.hot) {
+  console.log('✅  Server-side HMR Enabled!');
+
+  module.hot.accept('./server', () => {
+    console.log('🔁  HMR Reloading `./server`...');
+
+    try {
+      app = require('./server').default;
+      server.removeListener('request', currentApp);
+      server.on('request', app);
+      currentApp = app;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
