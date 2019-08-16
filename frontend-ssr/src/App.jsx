6c6aa44d33168d81281016f4d2d5464fd18caaf8
Jiazhen Xie <fdf515@gmail.com>
2019-08-16 20:22:39 +0100

    Runnable razzle app

--- frontend-ssr/src/App.jsx
+++ frontend-ssr/src/App.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import Route from 'react-router-dom/Route';
-import Switch from 'react-router-dom/Switch';
-import Home from './Home';
-import './App.scss';
+import React, { Component, Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history'
+// import ReactGA from 'react-ga';
+import Nav from './components/nav/nav';
+import Footer from './components/footer/footer';
+import Posts from './components/posts/posts';
+import Post from './components/posts/post';
+import About from './components/about/about';
+import NotFound from './components/not-found/not-found';
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
-);
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+library.add(fab, faCheckSquare, faCoffee)
+
+// ReactGA.initialize('UA-50154241-2');
+
+// const history = createBrowserHistory();
+// history.listen((location, action) => {
+//   ReactGA.set({ page: location.pathname });
+//   ReactGA.pageview(location.pathname);
+// });
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Nav />
+        {/*<Switch history={history}>*/}
+        <Switch>
+          <Route exact path="/" component={Posts} />
+          <Route exact path="/posts" component={Posts} />
+          <Route exact path="/about" component={About} />
+          <Route path="/posts/:slug" component={Post} />
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
+      </Fragment>
+    );
+  }
+}
 
 export default App;
