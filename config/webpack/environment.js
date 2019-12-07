e2658e74aa391538e16928d60733c3f38c41dec8
Jiazhen Xie <fdf515@gmail.com>
2019-12-07 00:48:44 +0000

    Move all css and images to webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,30 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+})
+
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default'],
+    ActionCable: 'actioncable'
+  })
+)
+
+environment.loaders.append('expose-loader', {
+  test: require.resolve('jquery'),
+  use: [{
+      loader: 'expose-loader',
+      options: 'jQuery'
+  }, {
+      loader: 'expose-loader',
+      options: '$'
+  }]
+})
 
 module.exports = environment
