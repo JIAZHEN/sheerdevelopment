8888a72a221b5f48edb267b7005cbae4c6b147c6
Jiazhen Xie <jiazhen.xie@gousto.co.uk>
2021-09-05 20:51:11 +0100

    Add segment tracking

--- app/javascript/newlook/home.js
+++ app/javascript/newlook/home.js
@@ -1,42 +1,42 @@
-$(document).ready(function() {
-  $("#search").keyup(function(event){
-      if(event.keyCode == 13){
-          $("#search").submit();
-      }
+$(document).ready(function () {
+  $("#search").keyup(function (event) {
+    if (event.keyCode == 13) {
+      $("#search").submit();
+    }
   });
 
-
-  $('.grid').infinitescroll({
-    navSelector  : '.pagination',    // selector for the paged navigation
-    nextSelector : '.pagination .next a',  // selector for the NEXT link (to page 2)
-    itemSelector : '.post-item',     // selector for all items you'll retrieve
-    animate      : true,
+  $(".grid").infinitescroll({
+    navSelector: ".pagination", // selector for the paged navigation
+    nextSelector: ".pagination .next a", // selector for the NEXT link (to page 2)
+    itemSelector: ".post-item", // selector for all items you'll retrieve
+    animate: true,
     loading: {
       finishedMsg: "<em>Congratulations, you've reached the end.</em>",
       img: "#{asset_pack_path('media/images/loading.gif')}"
     },
     // trigger Masonry as a callback
-    function( newElements ) {
+    function(newElements) {
       // hide new items while they are loading
-      var $newElems = $( newElements ).css({ opacity: 0 });
+      var $newElems = $(newElements).css({ opacity: 0 });
       // ensure that images load before adding to masonry layout
-      $newElems.imagesLoaded(function(){
+      $newElems.imagesLoaded(function () {
         // show elems now they're ready
         $newElems.animate({ opacity: 1 });
       });
     }
   });
 
-  if($(".display-disqus").length) {
+  if ($(".display-disqus").length) {
     var disqus_config = function () {
       this.page.url = "#{request.original_url}";
       this.page.identifier = "#{@post.disqus_identifier}";
     };
 
-    (function() {
-      var d = document, s = d.createElement('script');
-      s.src = '//sheerdevelopment.disqus.com/embed.js';
-      s.setAttribute('data-timestamp', +new Date());
+    (function () {
+      var d = document,
+        s = d.createElement("script");
+      s.src = "//sheerdevelopment.disqus.com/embed.js";
+      s.setAttribute("data-timestamp", +new Date());
       (d.head || d.body).appendChild(s);
     })();
   }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,20 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
 
-require("newlook/styles/styles")
-require("bootstrap/dist/js/bootstrap")
-require("newlook/jquery.infinitescroll.min")
-require("jquery-steps/build/jquery.steps.min")
-require("imagesloaded")
-require("newlook/ga")
-require("newlook/home")
+require("newlook/styles/styles");
+require("bootstrap/dist/js/bootstrap");
+require("newlook/jquery.infinitescroll.min");
+require("jquery-steps/build/jquery.steps.min");
+require("imagesloaded");
+require("newlook/ga");
+require("newlook/home");
+require("newlook/segment");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-require.context('../images', true)
+require.context("../images", true);
 // const imagePath = (name) => images(name, true)
